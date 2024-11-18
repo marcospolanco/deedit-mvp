8a82ec3ef95b874da89913cd576fa5c0458556b1
Marcos Polanco <marcospolanco@users.noreply.github.com>
2024-11-18 08:07:05 +0000

    removed upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -4,14 +4,14 @@ import { SimpleUploadButton } from "./simple-upload-button";
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <div>Deedit</div>
 
       <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <SimpleUploadButton />
+          {/* <SimpleUploadButton /> */}
           <UserButton />
         </SignedIn>
       </div>
