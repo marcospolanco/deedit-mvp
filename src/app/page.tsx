2679b0c96f4cd177e588d030dd8b3eed1f533c29
Marcos Polnaco <mjpolanco@gmail.com>
2024-12-09 22:15:51 -0800

    remove padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function HomePage() {
     <main className="bg-[#ffe9bb]">
       <SignedOut>
         <div className="h-full w-[80%] mx-auto text-center text-2xl flex items-center justify-center">
-          <div className="flex flex-col items-center pt-8 gap-2">
+          <div className="flex flex-col items-center">
           <img src='https://utfs.io/f/WJjSPLaJDmw9CGPIFIuOHq2ldW1GRYgtVSwMLT3XzP4FDcQE' alt="Deedit Logo" className="w-[30vw] max-w-[120px] h-auto object-contain" />
           <div className="rounded-lg p-2 w-[70%] mx-auto flex">
               <div className="p-4">
